0b5d8b70565196c6f89ce9902c8e6129e7b1c073
Aslıhan Şimşek <simseekasli@gmail.com>
2022-05-04 18:07:50 +0300

    -How many number will your enter -Create array -Find max and min number with array max min function

--- FindMaxMinNumber/src/FindMaxMinNumber.java
+++ FindMaxMinNumber/src/FindMaxMinNumber.java
@@ -1,5 +1,23 @@
+import sun.security.util.ArrayUtil;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class FindMaxMinNumber {
     public static void main(String[] args) {
-        System.out.println("hello");
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("How many number will you enter ..:");
+        int numberCount = scanner.nextInt();
+
+        int[] numberArray = new int[numberCount];
+
+        for (int i = 0; i < numberCount; i++) {
+            System.out.print("Enter "+ (i + 1) + ". Number..:");
+            numberArray[i] = scanner.nextInt();
+        }
+
+        System.out.println("Max Number ..:" + Arrays.stream(numberArray).max());
+        System.out.println("Min Number ..:" + Arrays.stream(numberArray).min());
     }
 }
